3701135842fbaf47c5b3ff7e530ae76e8ce5cf24
houssein.maatouk <houssein.maatouk@nsgroup.ch>
2018-09-15 13:14:57 +0200

    add static wallet page

--- app/src/pages/wallet/wallet.ts
+++ app/src/pages/wallet/wallet.ts
@@ -8,11 +8,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class WalletPage {
 
+  mainWallet = {currency: 'EUR', amount: 50.00, is_main: true};
+
+  wallets = [
+      {
+        currency: 'USD', 
+        amount: 53.00,
+        pending: [
+          {
+            currency: 'CHF', 
+            amount: 53.00,
+            until: '2019-10-10'
+          },
+          {
+            currency: 'EUR', 
+            amount: 53.00,
+            until: '2020-10-10'
+          }
+        ]
+      },
+      {currency: 'CHF', amount: 60.00},
+  ]
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad WalletPage');
+  
   }
 
 }
