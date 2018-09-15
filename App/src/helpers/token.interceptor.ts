d5bc010f0dc4ac9ddc7ccd77bc63772f094a2e2f
D4rkMindz <bjoern.pfoster@gmail.com>
2018-09-15 19:06:35 +0200

    Fixed token interceptor

--- App/src/helpers/token.interceptor.ts
+++ App/src/helpers/token.interceptor.ts
@@ -5,11 +5,11 @@ export class TokenInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
-    const regex = /^fixer$/gi;
+    const regex = /.*fixer.*/gi;
     let headers = null;
     const match = regex.test(request.url.toString());
     console.log('regex match', match);
-    if (match) {
+    if (!match) {
       headers = request.headers.append('Content-Type', 'application/json');
       request = request.clone({headers: headers});
       if (token) {
