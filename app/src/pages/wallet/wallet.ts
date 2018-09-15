cd17a25d93321d82e6b8ebc6def651d68ce07609
D4rkMindz <bjoern.pfoster@gmail.com>
2018-09-15 19:13:31 +0200

    Added logout

--- app/src/pages/wallet/wallet.ts
+++ app/src/pages/wallet/wallet.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { App, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
-import { Headers } from '@angular/http';
+import { DepositPage } from "../deposit/deposit";
+import { LoginPage } from "../login/login";
+import { AuthProvider } from "../../providers/auth/auth";
 
 @Component({
   selector: 'page-wallet',
@@ -18,33 +20,41 @@ export class WalletPage {
       amount: number;
       until: string;
     };
-  }
+  };
 
   private walletApi = 'http://radiant-earth-58477.herokuapp.com/wallets';
 
   mainWallet = {currency: 'EUR', amount: 50.00, is_main: true};
 
   wallets = [
-      {
-        currency: 'USD', 
-        amount: 53.00,
-        pending: [
-          {
-            currency_to: 'CHF', 
-            amount: 53.00,
-            until: '2019-10-10'
-          },
-          {
-            currency_to: 'EUR', 
-            amount: 53.00,
-            until: '2020-10-10'
-          }
-        ]
-      },
-      {currency: 'CHF', amount: 60.00},
-  ]
+    {
+      currency: 'USD',
+      amount: 53.00,
+      pending: [
+        {
+          currency_to: 'CHF',
+          amount: 53.00,
+          until: '2019-10-10'
+        },
+        {
+          currency_to: 'EUR',
+          amount: 53.00,
+          until: '2020-10-10'
+        }
+      ]
+    },
+    {currency: 'CHF', amount: 60.00},
+  ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public http: HttpClient,
+              public app: App,
+              private auth: AuthProvider) {
+    if (!this.auth.isAuthenticated()) {
+      this.app.getRootNav().setRoot(LoginPage);
+      return;
+    }
   }
 
   ionViewDidLoad() {
@@ -57,6 +67,15 @@ export class WalletPage {
         console.log(response);
       }
     );
+  }
+
+  createDeposit() {
+    this.navCtrl.push(DepositPage);
+  }
+
+  public logout() {
+    this.auth.logout();
+    this.app.getRootNav().setRoot(LoginPage);
   }
 
 }
