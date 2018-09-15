a8fe030604227558f7bd7ceede17040195eea130
houssein.maatouk <houssein.maatouk@nsgroup.ch>
2018-09-15 18:58:34 +0200

    make some corrections

--- app/src/pages/wallet/wallet.ts
+++ app/src/pages/wallet/wallet.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { Headers } from '@angular/http';
 
 @Component({
   selector: 'page-wallet',
   templateUrl: 'wallet.html',
 })
 export class WalletPage {
+
+  walletModel: {
+    currency: string;
+    amount: number;
+    is_true: boolean;
+    pending: {
+      currency_to: string;
+      amount: number;
+      until: string;
+    };
+  }
+
+  private walletApi = 'http://radiant-earth-58477.herokuapp.com/wallets';
 
   mainWallet = {currency: 'EUR', amount: 50.00, is_main: true};
 
@@ -16,12 +30,12 @@ export class WalletPage {
         amount: 53.00,
         pending: [
           {
-            currency: 'CHF', 
+            currency_to: 'CHF', 
             amount: 53.00,
             until: '2019-10-10'
           },
           {
-            currency: 'EUR', 
+            currency_to: 'EUR', 
             amount: 53.00,
             until: '2020-10-10'
           }
@@ -34,7 +48,15 @@ export class WalletPage {
   }
 
   ionViewDidLoad() {
-  
+    this.getWalletsData();
+  }
+
+  getWalletsData(): any {
+    return this.http.get(this.walletApi).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    );
   }
 
 }
