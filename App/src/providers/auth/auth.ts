b1cdc8c57b4e650bf6c67bbfdb652eaa49253cd4
D4rkMindz <bjoern.pfoster@gmail.com>
2018-09-15 16:40:21 +0200

    Removed catchError

--- App/src/providers/auth/auth.ts
+++ App/src/providers/auth/auth.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from "../../environment/environment";
-import { catchError, map } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 
 @Injectable()
